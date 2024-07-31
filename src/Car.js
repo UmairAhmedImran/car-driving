eb06e6968d5e1ecf4698cdb5df9ae3239f189498
unknown <umairahmedimranbutt@gmail.com>
2024-07-31 17:29:49 +0500

    Adding the model

---
+++ src/Car.js
@@ -0,0 +1,25 @@
+import { useLoader } from "@react-three/fiber";
+import { useEffect } from "react";
+import { Mesh } from "three";
+import { GLTFLoader } from "three/examples/jsm/Addons.js";
+
+export function Car() {
+  const gltf = useLoader(
+    GLTFLoader,
+    process.env.PUBLIC_URL + "models/car/scene.gltf"
+  );
+
+  useEffect(() => {
+    gltf.scene.scale.set(0.005, 0.005, 0.005);
+    gltf.scene.position.set(0, -0.035, 0);
+    gltf.scene.traverse((object) => {
+      if (object instanceof Mesh) {
+        object.castShadow = true;
+        object.receiveShadow = true;
+        object.material.envMapIntensity = 20;
+      }
+    });
+  }, [gltf]);
+
+  return <primitive object={gltf.scene} />;
+}
